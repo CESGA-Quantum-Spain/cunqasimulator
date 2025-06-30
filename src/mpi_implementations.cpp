841884e9a0fc2506631e17507a465e5906ebbe09
acarballido <varocarballido1996@gmail.com>
2025-06-30 19:11:57 +0200

    Realizing that the implementation was not correct

--- src/mpi_implementations.cpp
+++ src/mpi_implementations.cpp
@@ -29,37 +29,37 @@ meas_out mpi_cunqa_apply_measure(StateVector& statevector, std::vector<int> qubi
     res.measure = 0;
     return res;
 }
+
+
 // One-Qubit Gates
 void mpi_cunqa_apply_x(StateVector& statevector, const std::vector<int> qubits, const int& mpi_rank) 
 {
-    if (qubits[0] <= 35 && mpi_rank == 0) {
+    if (qubits[0] <= 35 ) {
         cunqa_apply_x(statevector, qubits);
     } else if (qubits[0] > 35) {
         MPI_Request send_request;
         MPI_Request recv_request;
+        MPI_Status status;
+
         std::complex<double>* send_chunk_ptr;
         std::complex<double>* recv_chunk_ptr;
         int comm;
         StateVector aux_statevector(chunk_elements);
-        std::complex<double> aux;
-        /* int send_completed = 0;
-        int recv_completed = 0; */
-        MPI_Status status;
+
         int N = qubits[0] - 35;
-        if (mpi_rank < (1 << N)) {
-            for (uint64_t i = 0; i < num_chunks; ++i) {
-                send_chunk_ptr = statevector.data() + (i * chunk_elements);
-                recv_chunk_ptr = aux_statevector.data();
-                comm = (mpi_rank + (1 << (N - 1))) % (1 << N);
-                MPI_Isend(send_chunk_ptr, chunk_elements, MPI_CXX_DOUBLE_COMPLEX, comm, 0, MPI_COMM_WORLD, &send_request);
-                MPI_Irecv(recv_chunk_ptr, chunk_elements, MPI_CXX_DOUBLE_COMPLEX, comm, 0, MPI_COMM_WORLD, &recv_request);
+        //TODO
+        for (uint64_t i = 0; i < num_chunks; ++i) {
+            send_chunk_ptr = statevector.data() + (i * chunk_elements);
+            recv_chunk_ptr = aux_statevector.data();
+            comm = (mpi_rank + (1 << (N - 1))) % (1 << N);
+            MPI_Isend(send_chunk_ptr, chunk_elements, MPI_CXX_DOUBLE_COMPLEX, comm, 0, MPI_COMM_WORLD, &send_request);
+            MPI_Irecv(recv_chunk_ptr, chunk_elements, MPI_CXX_DOUBLE_COMPLEX, comm, 0, MPI_COMM_WORLD, &recv_request);
 
-                MPI_Wait(&send_request, &status); 
-                MPI_Wait(&recv_request, &status);
+            MPI_Wait(&send_request, &status); 
+            MPI_Wait(&recv_request, &status);
 
-                for (uint64_t j = 0; j < chunk_elements; j++) {
-                    statevector[j + (i * chunk_elements)] = recv_chunk_ptr[j];
-                }
+            for (uint64_t j = 0; j < chunk_elements; j++) {
+                statevector[j + (i * chunk_elements)] = recv_chunk_ptr[j];
             }
         }
     }
@@ -68,27 +68,284 @@ void mpi_cunqa_apply_x(StateVector& statevector, const std::vector<int> qubits, 
 
 void mpi_cunqa_apply_y(StateVector& statevector, const std::vector<int> qubits, const int& mpi_rank) 
 {
-    
+    if (qubits[0] <= 35) {
+        cunqa_apply_y(statevector, qubits);
+    } else if (qubits[0] > 35) {
+        MPI_Request send_request;
+        MPI_Request recv_request;
+        MPI_Status status;
+
+        std::complex<double>* send_chunk_ptr;
+        std::complex<double>* recv_chunk_ptr;
+        int comm;
+        StateVector aux_statevector(chunk_elements);
+
+        int N = qubits[0] - 35;
+        for (uint64_t i = 0; i < num_chunks; ++i) {
+            send_chunk_ptr = statevector.data() + (i * chunk_elements);
+            recv_chunk_ptr = aux_statevector.data();
+            comm = (mpi_rank + (1 << (N - 1))) % (1 << N);
+            MPI_Isend(send_chunk_ptr, chunk_elements, MPI_CXX_DOUBLE_COMPLEX, comm, 0, MPI_COMM_WORLD, &send_request);
+            MPI_Irecv(recv_chunk_ptr, chunk_elements, MPI_CXX_DOUBLE_COMPLEX, comm, 0, MPI_COMM_WORLD, &recv_request);
+
+            MPI_Wait(&send_request, &status); 
+            MPI_Wait(&recv_request, &status);
+
+            if (mpi_rank > ((1 << (qubits[0] - 35)) - 1)) { // 1 in position qubits[0]
+                for (uint64_t j = 0; j < chunk_elements; j++) {
+                    statevector[j + (i * chunk_elements)] = -imag * recv_chunk_ptr[j];
+                }
+            } else {
+                for (uint64_t j = 0; j < chunk_elements; j++) {
+                    statevector[j + (i * chunk_elements)] = imag * recv_chunk_ptr[j];
+                }
+            }
+        }
+    }
 }
 
 
 void mpi_cunqa_apply_z(StateVector& statevector, const std::vector<int> qubits, const int& mpi_rank) 
 {
-  
+
+    if (qubits[0] <= 35) {
+        cunqa_apply_z(statevector, qubits);
+    } else if (qubits[0] > 35 && mpi_rank >= ((1 << (qubits[0] - 35)) - 1)) {
+        for (uint64_t i = 0; i < statevector.size(); i++) {
+            statevector[i] = -statevector[i];
+        }
+    }
 }
 
 
 void mpi_cunqa_apply_h(StateVector& statevector, const std::vector<int> qubits, const int& mpi_rank) 
 {
-    
+    if (qubits[0] <= 35) {
+        cunqa_apply_h(statevector, qubits);
+    } else if (qubits[0] > 35) {
+        MPI_Request send_request;
+        MPI_Request recv_request;
+        MPI_Status status;
+
+        std::complex<double>* send_chunk_ptr;
+        std::complex<double>* recv_chunk_ptr;
+        int comm;
+        StateVector aux_statevector(chunk_elements);
+
+        int N = qubits[0] - 35;
+        for (uint64_t i = 0; i < num_chunks; ++i) {
+            send_chunk_ptr = statevector.data() + (i * chunk_elements);
+            recv_chunk_ptr = aux_statevector.data();
+            comm = (mpi_rank + (1 << (N - 1))) % (1 << N);
+            MPI_Isend(send_chunk_ptr, chunk_elements, MPI_CXX_DOUBLE_COMPLEX, comm, 0, MPI_COMM_WORLD, &send_request);
+            MPI_Irecv(recv_chunk_ptr, chunk_elements, MPI_CXX_DOUBLE_COMPLEX, comm, 0, MPI_COMM_WORLD, &recv_request);
+
+            MPI_Wait(&send_request, &status); 
+            MPI_Wait(&recv_request, &status);
+
+            if (mpi_rank > ((1 << (qubits[0] - 35)) - 1)) { // 1 in position qubits[0]
+                for (uint64_t j = 0; j < chunk_elements; j++) {
+                    statevector[j + (i * chunk_elements)] = inverse_sqrt_2 * recv_chunk_ptr[j] - inverse_sqrt_2 * statevector[j + (i * chunk_elements)];
+                }
+            } else {
+                for (uint64_t j = 0; j < chunk_elements; j++) {
+                    statevector[j + (i * chunk_elements)] = inverse_sqrt_2 * statevector[j + (i * chunk_elements)] + inverse_sqrt_2 * recv_chunk_ptr[j];
+                }
+            }
+        }
+    }
 }
 
 
-void mpi_cunqa_apply_sx(StateVector& statevector, const std::vector<int> qubits, const int& mpi_rank) {}
+void mpi_cunqa_apply_sx(StateVector& statevector, const std::vector<int> qubits, const int& mpi_rank) 
+{
+    if (qubits[0] <= 35) {
+        cunqa_apply_sx(statevector, qubits);
+    } else if (qubits[0] > 35) {
+        MPI_Request send_request;
+        MPI_Request recv_request;
+        MPI_Status status;
+
+        std::complex<double>* send_chunk_ptr;
+        std::complex<double>* recv_chunk_ptr;
+        int comm;
+        StateVector aux_statevector(chunk_elements);
+        double a_half = (double)1.0/(double)2.0;
+
+        int N = qubits[0] - 35;
+        for (uint64_t i = 0; i < num_chunks; ++i) {
+            send_chunk_ptr = statevector.data() + (i * chunk_elements);
+            recv_chunk_ptr = aux_statevector.data();
+            comm = (mpi_rank + (1 << (N - 1))) % (1 << N);
+            MPI_Isend(send_chunk_ptr, chunk_elements, MPI_CXX_DOUBLE_COMPLEX, comm, 0, MPI_COMM_WORLD, &send_request);
+            MPI_Irecv(recv_chunk_ptr, chunk_elements, MPI_CXX_DOUBLE_COMPLEX, comm, 0, MPI_COMM_WORLD, &recv_request);
+
+            MPI_Wait(&send_request, &status); 
+            MPI_Wait(&recv_request, &status);
+
+            if (mpi_rank > ((1 << (qubits[0] - 35)) - 1)) { // 1 in position qubits[0]
+                for (uint64_t j = 0; j < chunk_elements; j++) {
+                    statevector[j + (i * chunk_elements)] = a_half * (1.0 + imag) * recv_chunk_ptr[j] + a_half * (1.0 - imag) * statevector[j + (i * chunk_elements)];
+                }
+            } else {
+                for (uint64_t j = 0; j < chunk_elements; j++) {
+                    statevector[j + (i * chunk_elements)] = a_half * (1.0 + imag) * statevector[j + (i * chunk_elements)] + a_half * (1.0 - imag) * recv_chunk_ptr[j];
+                }
+            }
+        }
+    }
+}
+
+
+void mpi_cunqa_apply_rx(StateVector& statevector, const std::vector<int> qubits, const Params& param, const int& mpi_rank) 
+{
+    if (qubits[0] <= 35) {
+        cunqa_apply_rx(statevector, qubits, param);
+    } else if (qubits[0] > 35) {
+        MPI_Request send_request;
+        MPI_Request recv_request;
+        MPI_Status status;
+
+        std::complex<double>* send_chunk_ptr;
+        std::complex<double>* recv_chunk_ptr;
+        int comm;
+        StateVector aux_statevector(chunk_elements);
+        double sin = std::sin(param[0]/2.0);
+        double cos = std::cos(param[0]/2.0);
+
+        int N = qubits[0] - 35;
+        for (uint64_t i = 0; i < num_chunks; ++i) {
+            send_chunk_ptr = statevector.data() + (i * chunk_elements);
+            recv_chunk_ptr = aux_statevector.data();
+            comm = (mpi_rank + (1 << (N - 1))) % (1 << N);
+            MPI_Isend(send_chunk_ptr, chunk_elements, MPI_CXX_DOUBLE_COMPLEX, comm, 0, MPI_COMM_WORLD, &send_request);
+            MPI_Irecv(recv_chunk_ptr, chunk_elements, MPI_CXX_DOUBLE_COMPLEX, comm, 0, MPI_COMM_WORLD, &recv_request);
+
+            MPI_Wait(&send_request, &status); 
+            MPI_Wait(&recv_request, &status);
+
+            for (uint64_t j = 0; j < chunk_elements; j++) {
+                statevector[j + (i * chunk_elements)] = cos * statevector[j + (i * chunk_elements)] - imag * sin * recv_chunk_ptr[j];
+            }
+        }
+    }
+}
+
+
+void mpi_cunqa_apply_ry(StateVector& statevector, const std::vector<int> qubits, const Params& param, const int& mpi_rank) 
+{
+    if (qubits[0] <= 35) {
+        cunqa_apply_ry(statevector, qubits, param);
+    } else if (qubits[0] > 35) {
+        MPI_Request send_request;
+        MPI_Request recv_request;
+        MPI_Status status;
+
+        std::complex<double>* send_chunk_ptr;
+        std::complex<double>* recv_chunk_ptr;
+        int comm;
+        StateVector aux_statevector(chunk_elements);
+        double sin = std::sin(param[0]/2.0);
+        double cos = std::cos(param[0]/2.0);
+
+        int N = qubits[0] - 35;
+        for (uint64_t i = 0; i < num_chunks; ++i) {
+            send_chunk_ptr = statevector.data() + (i * chunk_elements);
+            recv_chunk_ptr = aux_statevector.data();
+            comm = (mpi_rank + (1 << (N - 1))) % (1 << N);
+            MPI_Isend(send_chunk_ptr, chunk_elements, MPI_CXX_DOUBLE_COMPLEX, comm, 0, MPI_COMM_WORLD, &send_request);
+            MPI_Irecv(recv_chunk_ptr, chunk_elements, MPI_CXX_DOUBLE_COMPLEX, comm, 0, MPI_COMM_WORLD, &recv_request);
+
+            MPI_Wait(&send_request, &status); 
+            MPI_Wait(&recv_request, &status);
+
+            if (mpi_rank > ((1 << (qubits[0] - 35)) - 1)) { // 1 in position qubits[0]
+                for (uint64_t j = 0; j < chunk_elements; j++) {
+                    statevector[j + (i * chunk_elements)] = cos * statevector[j + (i * chunk_elements)] - sin * recv_chunk_ptr[j];
+                }
+            } else {
+                for (uint64_t j = 0; j < chunk_elements; j++) {
+                    statevector[j + (i * chunk_elements)] = cos * statevector[j + (i * chunk_elements)] + sin * recv_chunk_ptr[j];
+                }
+            }
+        }
+    }
+}
+
+
+void mpi_cunqa_apply_rz(StateVector& statevector, const std::vector<int> qubits, const Params& param, const int& mpi_rank) 
+{
+    if (qubits[0] <= 35) {
+        cunqa_apply_rz(statevector, qubits, param);
+    } else if (qubits[0] > 35 && mpi_rank < ((1 << (qubits[0] - 35)) - 1)) {
+        double sin = std::sin(param[0]/2.0);
+        double cos = std::cos(param[0]/2.0);
+        for (uint64_t i = 0; i < statevector.size(); i++) {
+            statevector[i] = (cos - imag * sin) * statevector[i];
+        }
+    } else if (qubits[0] > 35 && mpi_rank >= ((1 << (qubits[0] - 35)) - 1)) {
+        double sin = std::sin(param[0]/2.0);
+        double cos = std::cos(param[0]/2.0);
+        for (uint64_t i = 0; i < statevector.size(); i++) {
+            statevector[i] = (cos + imag * sin) * statevector[i];
+        }
+    }
+}
+
+
 // Two-Qubit Gates
-void mpi_cunqa_apply_cx(StateVector& statevector, const std::vector<int> qubits, const int& mpi_rank) {}
+void mpi_cunqa_apply_cx(StateVector& statevector, const std::vector<int> qubits, const int& mpi_rank) 
+{
+    if (qubits[0] <= 35 && qubits[0] <= 35) {
+        cunqa_apply_cx(statevector, qubits, param);
+    } else if (qubits[0] <= 35 && qubits[0] > 35) {
+        MPI_Request send_request;
+        MPI_Request recv_request;
+        MPI_Status status;
+
+        std::complex<double>* send_chunk_ptr;
+        std::complex<double>* recv_chunk_ptr;
+        int comm;
+        StateVector aux_statevector(chunk_elements); //TODO
+        double sin = std::sin(param[0]/2.0);
+        double cos = std::cos(param[0]/2.0);
+
+        int N = qubits[1] - 35;
+        int M = n_qubits - 35;
+        for (uint64_t i = 0; i < num_chunks; ++i) {
+            send_chunk_ptr = statevector.data() + (i * chunk_elements); //TODO
+            recv_chunk_ptr = aux_statevector.data();
+            comm = (mpi_rank + (1 << (N - 1))) % (1 << N);
+            MPI_Isend(send_chunk_ptr, chunk_elements, MPI_CXX_DOUBLE_COMPLEX, comm, 0, MPI_COMM_WORLD, &send_request);
+            MPI_Irecv(recv_chunk_ptr, chunk_elements, MPI_CXX_DOUBLE_COMPLEX, comm, 0, MPI_COMM_WORLD, &recv_request);
+
+            MPI_Wait(&send_request, &status); 
+            MPI_Wait(&recv_request, &status);
+
+            if (mpi_rank > ((1 << (qubits[0] - 35)) - 1)) { // 1 in position qubits[0]
+                for (uint64_t j = 0; j < chunk_elements; j++) {
+                    statevector[j + (i * chunk_elements)] = -imag * recv_chunk_ptr[j];
+                }
+            } else {
+                for (uint64_t j = 0; j < chunk_elements; j++) {
+                    statevector[j + (i * chunk_elements)] = imag * recv_chunk_ptr[j];
+                }
+            }
+        }
+
+    } else if (qubits[0] > 35 && qubits[0] <= 35) {
+
+    } else if (qubits[0] > 35 && qubits[0] > 35) {
+
+    }
+}
+
+
 void mpi_cunqa_apply_cy(StateVector& statevector, const std::vector<int> qubits, const int& mpi_rank) {}
 void mpi_cunqa_apply_cz(StateVector& statevector, const std::vector<int> qubits, const int& mpi_rank) {}
+void mpi_cunqa_apply_crx(StateVector& statevector, const std::vector<int> qubits, const Params& param, const int& mpi_rank) {}
+void mpi_cunqa_apply_cry(StateVector& statevector, const std::vector<int> qubits, const Params& param, const int& mpi_rank) {}
+void mpi_cunqa_apply_crz(StateVector& statevector, const std::vector<int> qubits, const Params& param, const int& mpi_rank) {}
 void mpi_cunqa_apply_ecr(StateVector& statevector, const std::vector<int> qubits, const int& mpi_rank) {}
 //Classical conditional one-qubit gates
 void mpi_cunqa_apply_cifx(StateVector& statevector, const std::vector<int> qubits, const int& mpi_rank) {}
@@ -96,20 +353,14 @@ void mpi_cunqa_apply_cify(StateVector& statevector, const std::vector<int> qubit
 void mpi_cunqa_apply_cifz(StateVector& statevector, const std::vector<int> qubits, const int& mpi_rank) {}
 void mpi_cunqa_apply_cifh(StateVector& statevector, const std::vector<int> qubits, const int& mpi_rank) {}
 void mpi_cunqa_apply_cifsx(StateVector& statevector, const std::vector<int> qubits, const int& mpi_rank) {}
+void mpi_cunqa_apply_cifrx(StateVector& statevector, const std::vector<int> qubits, const Params& param, const int& mpi_rank) {}
+void mpi_cunqa_apply_cifry(StateVector& statevector, const std::vector<int> qubits, const Params& param, const int& mpi_rank) {}
+void mpi_cunqa_apply_cifrz(StateVector& statevector, const std::vector<int> qubits, const Params& param, const int& mpi_rank) {}
 //Classical conditional two-qubits gates
 void mpi_cunqa_apply_cifcx(StateVector& statevector, const std::vector<int> qubits, const int& mpi_rank) {}
 void mpi_cunqa_apply_cifcy(StateVector& statevector, const std::vector<int> qubits, const int& mpi_rank) {}
 void mpi_cunqa_apply_cifcz(StateVector& statevector, const std::vector<int> qubits, const int& mpi_rank) {}
 void mpi_cunqa_apply_cifecr(StateVector& statevector, const std::vector<int> qubits, const int& mpi_rank) {}
-void mpi_cunqa_apply_rx(StateVector& statevector, const std::vector<int> qubits, const Params& param, const int& mpi_rank) {}
-void mpi_cunqa_apply_ry(StateVector& statevector, const std::vector<int> qubits, const Params& param, const int& mpi_rank) {}
-void mpi_cunqa_apply_rz(StateVector& statevector, const std::vector<int> qubits, const Params& param, const int& mpi_rank) {}
-void mpi_cunqa_apply_crx(StateVector& statevector, const std::vector<int> qubits, const Params& param, const int& mpi_rank) {}
-void mpi_cunqa_apply_cry(StateVector& statevector, const std::vector<int> qubits, const Params& param, const int& mpi_rank) {}
-void mpi_cunqa_apply_crz(StateVector& statevector, const std::vector<int> qubits, const Params& param, const int& mpi_rank) {}
-void mpi_cunqa_apply_cifrx(StateVector& statevector, const std::vector<int> qubits, const Params& param, const int& mpi_rank) {}
-void mpi_cunqa_apply_cifry(StateVector& statevector, const std::vector<int> qubits, const Params& param, const int& mpi_rank) {}
-void mpi_cunqa_apply_cifrz(StateVector& statevector, const std::vector<int> qubits, const Params& param, const int& mpi_rank) {}
 // Personalized gates
 void mpi_cunqa_apply_1_gate(const Matrix& U, StateVector& statevector, const std::vector<int> qubits, const int& mpi_rank) {}
 void mpi_cunqa_apply_2_gate(const Matrix& U, StateVector& statevector, const std::vector<int> qubits, const int& mpi_rank) {}
--- src/mpi_implementations.hpp
+++ src/mpi_implementations.hpp
@@ -12,10 +12,16 @@ void mpi_cunqa_apply_y(StateVector& statevector, const std::vector<int> qubits, 
 void mpi_cunqa_apply_z(StateVector& statevector, const std::vector<int> qubits, const int& mpi_rank);
 void mpi_cunqa_apply_h(StateVector& statevector, const std::vector<int> qubits, const int& mpi_rank);
 void mpi_cunqa_apply_sx(StateVector& statevector, const std::vector<int> qubits, const int& mpi_rank);
+void mpi_cunqa_apply_rx(StateVector& statevector, const std::vector<int> qubits, const Params& param, const int& mpi_rank);
+void mpi_cunqa_apply_ry(StateVector& statevector, const std::vector<int> qubits, const Params& param, const int& mpi_rank);
+void mpi_cunqa_apply_rz(StateVector& statevector, const std::vector<int> qubits, const Params& param, const int& mpi_rank);
 // Two-Qubit Gates
 void mpi_cunqa_apply_cx(StateVector& statevector, const std::vector<int> qubits, const int& mpi_rank);
 void mpi_cunqa_apply_cy(StateVector& statevector, const std::vector<int> qubits, const int& mpi_rank);
 void mpi_cunqa_apply_cz(StateVector& statevector, const std::vector<int> qubits, const int& mpi_rank);
+void mpi_cunqa_apply_crx(StateVector& statevector, const std::vector<int> qubits, const Params& param, const int& mpi_rank);
+void mpi_cunqa_apply_cry(StateVector& statevector, const std::vector<int> qubits, const Params& param, const int& mpi_rank);
+void mpi_cunqa_apply_crz(StateVector& statevector, const std::vector<int> qubits, const Params& param, const int& mpi_rank);
 void mpi_cunqa_apply_ecr(StateVector& statevector, const std::vector<int> qubits, const int& mpi_rank);
 //Classical conditional one-qubit gates
 void mpi_cunqa_apply_cifx(StateVector& statevector, const std::vector<int> qubits, const int& mpi_rank);
@@ -23,20 +29,14 @@ void mpi_cunqa_apply_cify(StateVector& statevector, const std::vector<int> qubit
 void mpi_cunqa_apply_cifz(StateVector& statevector, const std::vector<int> qubits, const int& mpi_rank);
 void mpi_cunqa_apply_cifh(StateVector& statevector, const std::vector<int> qubits, const int& mpi_rank);
 void mpi_cunqa_apply_cifsx(StateVector& statevector, const std::vector<int> qubits, const int& mpi_rank);
+void mpi_cunqa_apply_cifrx(StateVector& statevector, const std::vector<int> qubits, const Params& param, const int& mpi_rank);
+void mpi_cunqa_apply_cifry(StateVector& statevector, const std::vector<int> qubits, const Params& param, const int& mpi_rank);
+void mpi_cunqa_apply_cifrz(StateVector& statevector, const std::vector<int> qubits, const Params& param, const int& mpi_rank);
 //Classical conditional two-qubits gates
 void mpi_cunqa_apply_cifcx(StateVector& statevector, const std::vector<int> qubits, const int& mpi_rank);
 void mpi_cunqa_apply_cifcy(StateVector& statevector, const std::vector<int> qubits, const int& mpi_rank);
 void mpi_cunqa_apply_cifcz(StateVector& statevector, const std::vector<int> qubits, const int& mpi_rank);
 void mpi_cunqa_apply_cifecr(StateVector& statevector, const std::vector<int> qubits, const int& mpi_rank);
-void mpi_cunqa_apply_rx(StateVector& statevector, const std::vector<int> qubits, const Params& param, const int& mpi_rank);
-void mpi_cunqa_apply_ry(StateVector& statevector, const std::vector<int> qubits, const Params& param, const int& mpi_rank);
-void mpi_cunqa_apply_rz(StateVector& statevector, const std::vector<int> qubits, const Params& param, const int& mpi_rank);
-void mpi_cunqa_apply_crx(StateVector& statevector, const std::vector<int> qubits, const Params& param, const int& mpi_rank);
-void mpi_cunqa_apply_cry(StateVector& statevector, const std::vector<int> qubits, const Params& param, const int& mpi_rank);
-void mpi_cunqa_apply_crz(StateVector& statevector, const std::vector<int> qubits, const Params& param, const int& mpi_rank);
-void mpi_cunqa_apply_cifrx(StateVector& statevector, const std::vector<int> qubits, const Params& param, const int& mpi_rank);
-void mpi_cunqa_apply_cifry(StateVector& statevector, const std::vector<int> qubits, const Params& param, const int& mpi_rank);
-void mpi_cunqa_apply_cifrz(StateVector& statevector, const std::vector<int> qubits, const Params& param, const int& mpi_rank);
 // Personalized gates
 void mpi_cunqa_apply_1_gate(const Matrix& U, StateVector& statevector, const std::vector<int> qubits, const int& mpi_rank);
 void mpi_cunqa_apply_2_gate(const Matrix& U, StateVector& statevector, const std::vector<int> qubits, const int& mpi_rank);
